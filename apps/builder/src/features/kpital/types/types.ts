63445b8d4d5bfe94be26a4e054d3139d203dcb4e
Roylan Suarez <roylan.suarez@gmail.com>
2024-10-08 12:40:34 +0200

    update types

--- apps/builder/src/features/kpital/types/types.ts
+++ apps/builder/src/features/kpital/types/types.ts
@@ -1,17 +1,3 @@
-export interface StoredFlowInputs {
-  url: string
-  campaignId: string
-}
-
-export interface AllFlowInputs extends StoredFlowInputs {
-  password: string
-}
-
-export interface LoginInputs {
-  username: string
-  password: string
-}
-
 export interface InputsSaveFlow extends Record<string, string> {
   url: string
   campaignId: string
